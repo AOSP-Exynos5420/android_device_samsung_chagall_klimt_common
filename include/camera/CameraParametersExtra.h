4ae1ef2fa9c02b618aeb14bfd478945d168c360e
deadman96385 <seanhoyt963@gmail.com>
2017-03-26 12:28:50 -0500

    Fix open source camera wrapper

--- include/camera/CameraParametersExtra.h
+++ include/camera/CameraParametersExtra.h
@@ -1,4 +1,5 @@
 /*
+ * Copyright (C) 2016 The CyanogenMod Project
  * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,52 +16,8 @@
  */
 
 #define CAMERA_PARAMETERS_EXTRA_C \
- \
-void DurationTimer::start(void)  \
-{ \
-    gettimeofday(&mStartWhen, NULL); \
-} \
- \
-void DurationTimer::stop(void) \
-{ \
-    gettimeofday(&mStopWhen, NULL); \
-} \
- \
-long long DurationTimer::durationUsecs(void) const \
-{ \
-    return (long) subtractTimevals(&mStopWhen, &mStartWhen); \
-} \
- \
-/*static*/ long long DurationTimer::subtractTimevals(const struct timeval* ptv1, \
-    const struct timeval* ptv2) \
-{ \
-    long long stop  = ((long long) ptv1->tv_sec) * 1000000LL + \
-                      ((long long) ptv1->tv_usec); \
-    long long start = ((long long) ptv2->tv_sec) * 1000000LL + \
-                      ((long long) ptv2->tv_usec); \
-    return stop - start; \ \
-} \
- \
-/*static*/ void DurationTimer::addToTimeval(struct timeval* ptv, long usec) \
-{ \
-    if (usec < 0) { \
-        ALOG(LOG_WARN, "", "Negative values not supported in addToTimeval\n"); \
-        return; \
-    } \
- \
-    if (ptv->tv_usec >= 1000000) { \
-        ptv->tv_sec += ptv->tv_usec / 1000000; \
-        ptv->tv_usec %= 1000000; \
-    } \
- \
-    ptv->tv_usec += usec % 1000000; \
-    if (ptv->tv_usec >= 1000000) { \
-        ptv->tv_usec -= 1000000; \
-        ptv->tv_sec++; \
-    } \
-    ptv->tv_sec += usec / 1000000; \
-} \
- \
+\
+const char CameraParameters::PIXEL_FORMAT_YUV420SP_NV21[] = "nv21"; \
 const char CameraParameters::KEY_CITYID[] = "cityid"; \
 const char CameraParameters::KEY_WEATHER[] = "weather"; \
 const char CameraParameters::ISO_AUTO[] = "auto"; \
@@ -68,43 +25,29 @@ const char CameraParameters::ISO_100[] = "ISO100"; \
 const char CameraParameters::ISO_200[] = "ISO200"; \
 const char CameraParameters::ISO_400[] = "ISO400"; \
 const char CameraParameters::ISO_800[] = "ISO800"; \
- \
+\
 int CameraParameters::getInt64(const char *key __attribute__ ((unused))) const \
 { \
     return -1; \
 } \
- \
+\
 extern "C" { \
     void acquire_dvfs_lock(void) { } \
     void release_dvfs_lock(void) { } \
 } \
- \
+
 #define CAMERA_PARAMETERS_EXTRA_H \
- \
-class DurationTimer { \
-public: \
-    DurationTimer() {} \
-    ~DurationTimer() {} \
-    void start(); \
-    void stop(); \
-    long long durationUsecs() const; \
-    static long long subtractTimevals(const struct timeval* ptv1, \
-        const struct timeval* ptv2); \
-    static void addToTimeval(struct timeval* ptv, long usec); \
-private: \
-    struct timeval  mStartWhen; \
-    struct timeval  mStopWhen; \
-}; \
- \
-    int getInt64(const char *key) const; \
- \
-    static const char KEY_CITYID[]; \
-    static const char KEY_WEATHER[]; \
-    static const char ISO_AUTO[]; \
-    static const char ISO_100[]; \
-    static const char ISO_200[]; \
-    static const char ISO_400[]; \
-    static const char ISO_800[]; \
+\
+int getInt64(const char *key) const; \
+\
+static const char PIXEL_FORMAT_YUV420SP_NV21[]; \
+static const char KEY_CITYID[]; \
+static const char KEY_WEATHER[]; \
+static const char ISO_AUTO[]; \
+static const char ISO_100[]; \
+static const char ISO_200[]; \
+static const char ISO_400[]; \
+static const char ISO_800[]; \
     /* LAST_LINE OF CAMERA_PARAMETERS_EXTRA_H, every line before this one *MUST* have
      * a backslash \ at the end of the line or else everything will break.
      */
