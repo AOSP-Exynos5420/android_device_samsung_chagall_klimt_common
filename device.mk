129e69460297981199286f93ed5abe2e80eea11a
deadman96385 <seanhoyt963@gmail.com>
2017-03-29 11:59:06 -0500

    Disable consumerir for now

--- device.mk
+++ device.mk
@@ -48,7 +48,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
+#    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml \
     frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
@@ -135,8 +135,8 @@ PRODUCT_PACKAGES += \
     libstlport
 
 # IR Blaster
-PRODUCT_PACKAGES += \
-    consumerir.universal5420
+#PRODUCT_PACKAGES += \
+#    consumerir.universal5420
 
 # Fingerprint Sensor
 PRODUCT_PACKAGES += \
