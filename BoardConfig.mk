9cf540ee481e895f0d3a9b78aedaec3f795940d7
Sean Hoyt <seanhoyt963@gmail.com>
2017-03-29 22:55:10 -0400

    Remove charger suspend

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,6 @@ BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/samsung/chagalllte
 
 # Charging mode
-BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 BOARD_BATTERY_DEVICE_NAME := battery
